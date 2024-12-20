b3e0b44ec262ac4346e0925c10f619bf93aa9734
Harshax <basicleo787@gmail.com>
2024-12-20 19:07:04 +0530

    updated css

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,7 +2,8 @@ import * as React from "react";
 export declare const Capitalize: ({ str }: {
     str: string;
 }) => string;
-export declare const ChatPilotBot: ({ apiKey, apiUrl, }: {
+export declare const ChatPilotBot: ({ apiKey, apiUrl, xApiKey, }: {
     apiKey: string;
+    xApiKey: string;
     apiUrl?: string;
 }) => React.JSX.Element;
